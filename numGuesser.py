73261f012955e1471d2a6ed831a0b5721de69941
Eric Poe <eric.poe@gmail.com>
2013-05-13 21:09:57 -0500

    Renamed "guess" variable to "mean" since this is a word my daughter learned this year in her math class.

--- numGuesser.py
+++ numGuesser.py
@@ -7,7 +7,7 @@ highNum = 1000
 guessCounter = 0
 depth = ceil(log(highNum - lowNum,2))
 answer = 'h'
-guess = highNum
+mean = highNum
 
 print("Think of a number between {0} and {1} and I will try to guess it {2} guesses or less".format(lowNum, highNum, depth))
 print("If I guess too high, let me know by pressing the 'h' key.")
@@ -19,11 +19,11 @@ while guessCounter <= depth:
         print("Yay! I guessed it in {0} guesses!".format(guessCounter))
         break
     if answer == 'h':
-        highNum = guess
+        highNum = mean
     if answer == 'l':
-        lowNum = guess
+        lowNum = mean
 
-    guess = ceil((highNum + lowNum)/2)
+    mean = ceil((highNum + lowNum)/2) #definition of the mean of 2 numbers
 
-    answer = input("Is your number {0}? ".format(guess))
+    answer = input("Is your number {0}? ".format(mean))
     guessCounter += 1
